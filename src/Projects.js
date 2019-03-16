9d9f0cd17b5fd507d2ee5955870754e0d9e2a7fb
Olfa <maslah.olfa@gmail.com>
2019-03-16 19:26:19 -0400

    more style change

---
+++ src/Projects.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './header.css';
+import './global.css';
+import Nav from './Nav.js';
+import bublegame from './images/buble-game1.jpg';
+import eiffeltower from './images/eiffel-tower2.jpg';
+import trigame from './images/trigame1.jpg';
+import triviagame from './images/triviagame1.jpg';
+
+class Projects extends Component {
+	render() {
+		return (
+			<section className="projects">
+				<figure className="buble-game figure1">
+					<img src={bublegame} alt="my first game in javascript" />
+					<figcaption>
+						<h2>Bubble Game</h2>
+						<p>Are you fast enough!</p>
+						<button>Play</button>
+					</figcaption>
+				</figure>
+				<figure className="figure2 trigame">
+					<img src={trigame} alt="memory game" />
+					<figcaption>
+						<h2>TrigoGame</h2>
+						<p>Test your memory!</p>
+						<button>Play</button>
+					</figcaption >
+				</figure >
+				<figure className="figure3">
+					<img src={triviagame} alt="" />
+					<figcaption>
+						<h2>Trivia Game</h2>
+						<p>Test your computer & maths knowledge!</p>
+						<button>Play</button>
+					</figcaption>
+				</figure>
+				<figure className="figure4">
+					<img src="" alt="" />
+					<figcaption>
+						<h2></h2>
+						<p></p>
+						<button></button>
+					</figcaption>
+				</figure>
+				<figure className="eiffel-tower figure5">
+					<img src={eiffeltower} alt="eiffel tower seen by bottom" />
+				</figure>
+			</section >
+		)
+	}
+}
+export default Projects;
