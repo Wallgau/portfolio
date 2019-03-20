2d242cf61b1de0f19d1c4af8042ed3baeec7bd46
Olfa <maslah.olfa@gmail.com>
2019-03-20 14:15:34 -0400

    responsive change

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './footer.css';
+import './global.css';
 import './setup.css';
 
 const Footer = () => (
-	<footer>
+	<footer className="footer">
 		<p>&copy; CopyRight Olfa Maslah</p>
 		<p>email: maslah.olfa@gmail.com</p>
-		<div>
+		<div className="links">
 			<a href="https://www.linkedin.com/in/olfa-maslah-1868ba172/"><i class="fab fa-linkedin"></i></a>
 			<a href="https://twitter.com/OMaslah"><i class="fab fa-twitter-square"></i></a>
 			<a href="https://github.com/Wallgau?tab=repositories"><i class="fab fa-github-square"></i></a>
