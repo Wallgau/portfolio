fc2cfdd2ff526520c6ffc8ae612e3fdaaff99aa5
Olfa <maslah.olfa@gmail.com>
2019-03-20 08:56:13 -0400

    about

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ const About = () => (
 	<section className="about">
 		<div className="wrapper about-me" id="about">
 			<h2 className="part-title">About me</h2>
-			<p>I am a junior Web Developer and I have a particular love for JavaScript (JS) and React, it's the reason why I built this website with React.js. If you ask me where I see myself in five years, I will answer as a full stack Web Developer who is always up to date on new tech. I am also interested in AI and data science, so being very confident with Python, PHP and C will be a dream come true for me.</p>
+			<p>I am a junior Web Developer and I have a particular love for JavaScript (JS) and React, it's the reason why I built this website with React.js. If you ask me where I see myself in five years, I will answer as a full stack Web Developer who is always up to date on new tech. I am also interested in AI and data science, so being very confident with R, Python, Java, Scala and SQL will be a dream come true for me.</p>
 		</div>
 	</section>
 )
