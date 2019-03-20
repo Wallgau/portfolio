f5c3f47e7227d9d178fa1f1c2bff9a74eb8e8a01
Olfa <maslah.olfa@gmail.com>
2019-03-20 12:52:18 -0400

    add email

--- src/Footer.js
+++ src/Footer.js
@@ -5,6 +5,7 @@ import './setup.css';
 const Footer = () => (
 	<footer>
 		<p>&copy; CopyRight Olfa Maslah</p>
+		<p>email: maslah.olfa@gmail.com</p>
 		<div>
 			<a href="https://www.linkedin.com/in/olfa-maslah-1868ba172/"><i class="fab fa-linkedin"></i></a>
 			<a href="https://twitter.com/OMaslah"><i class="fab fa-twitter-square"></i></a>
